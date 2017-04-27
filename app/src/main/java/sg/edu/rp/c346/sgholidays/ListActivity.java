7bf10e3a972e019e910948d22aad069349fad823
Herwan Shah <shahherwan@hotmail.com>
2017-04-28 01:22:15 +0800

    Added Second Activity

--- app/src/main/java/sg/edu/rp/c346/sgholidays/ListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/sgholidays/ListActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.c346.sgholidays;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,24 +9,58 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 /**
  * Created by 14049561 on 27/4/2017.
  */
 
 public class ListActivity extends AppCompatActivity{
 
+    TextView tvTitle2;
+    ListView lvHoliday;
+    ArrayAdapter<holiday> aaHoliday;
+    ArrayList<holiday> alHoliday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        // objects can be retrieved from database
-        ArrayList<ClassName> objects = ...;
-        // Create the ArrayAdapter object.
-        ClassNameArrayAdapter adapter =
-                new ClassNameArrayAdapter(this, R.layout.row, objects);
-        ListView lv = (ListView) findViewById(R.id.lvFood);
-        lv.setAdapter(adapter);
+        setContentView(R.layout.actvity_listview);
+
+        tvTitle2 = (TextView)findViewById(R.id.tvTitle2);
+
+        Intent intentReceived = getIntent();
+        String strTitle = intentReceived.getStringExtra("Title");
+        tvTitle2.setText(strTitle);
+
+        lvHoliday = (ListView)findViewById(R.id.lvHoliday);
+
+        alHoliday = new ArrayList<holiday>();
+
+        aaHoliday = new HolidayAdapterArray(this, R.layout.row, alHoliday);
+        lvHoliday.setAdapter(aaHoliday);
+
+
+        alHoliday.add(new holiday( "New Year's Day", "01 Jan 2017", "newyear"));
+        alHoliday.add(new holiday( "Labour Day", "01 May 2017", "labourday"));
+
+        lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                holiday selectedEvent = alHoliday.get(position);
+
+                Toast.makeText(ListActivity.this, selectedEvent.getName()
+                                + " Date:" + selectedEvent.getDate(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 
 
